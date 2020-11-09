4d3e1110e28250f9c3bd268d5f30076d5184edda
odeimaiz <maiz@itis.swiss>
2020-11-09 14:27:44 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,35 +29,37 @@ function populateTable(tbl, data) {
         const rowData = data[i];
         const tr = tbl.insertRow();
         tr.style.textAlign = "left";
-        for (let j=0; j<4; j++) {
-            if (j===3) {
-                const proxyurl = "https://cors-anywhere.herokuapp.com/";
-                const url = "https://staging.osparc.io/v0/";
-                fetch(proxyurl + url)
-                    .then(response => {
-                        response.json()
-                            .then(resp => {
-                                console.log(resp);
-                                const td = tr.insertCell();
-                                const a = document.createElement("a");
-                                const linkText = document.createTextNode(rowData[j]);
-                                a.appendChild(linkText);
-                                a.title = rowData[j];
-                                a.href = url;
-                                td.appendChild(a);
-                            })
-                            .catch(() => {
-                                console.error("Can’t access " + url);
-                                const td = tr.insertCell();
-                                td.appendChild(document.createTextNode("-"));
-                            });
+        
+        let td = tr.insertCell(0);
+        td.appendChild(document.createTextNode(rowData["name"]));
+        td = tr.insertCell(1);
+        td.appendChild(document.createTextNode(rowData["fileType"]));
+        td = tr.insertCell(2);
+        td.appendChild(document.createTextNode(rowData["size"] + " B"));
+
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const url = "https://staging.osparc.io/v0/";
+        fetch(proxyurl + url)
+            .then(response => {
+                response.json()
+                    .then(resp => {
+                        console.log(resp);
+                        td = tr.insertCell(3);
+                        const a = document.createElement("a");
+                        const linkText = document.createTextNode("Open in oSparc");
+                        a.appendChild(linkText);
+                        a.title = "Open in oSparc";
+                        a.href = url;
+                        td.appendChild(a);
                     })
-                    .catch(() => console.error("Can’t access proxy"));
-            } else {
-                const td = tr.insertCell();
-                td.appendChild(document.createTextNode(rowData[j]));
-            }
-        }
+                    .catch(() => {
+                        console.error("Can’t access " + url);
+                        const td = tr.insertCell();
+                        td.appendChild(document.createTextNode("-"));
+                    });
+            })
+            .catch(() => console.error("Can’t access proxy"));
+
         tbdy.appendChild(tr);
     }
     tbl.appendChild(tbdy);
@@ -70,8 +72,21 @@ document.body.appendChild(header);
 const filesTable = createTable();
 document.body.appendChild(filesTable);
 
+// from portal
 const tableData = [
-    ["template.json", "Json", "51B", "Open in oSparc"]
+    {
+        "name":"template.json",
+        "path":"files/template.json",
+        "size":51,
+        "icon":"JSON",
+        "uri":"s3://blackfynn-discover-use1/84/1/files/template.json",
+        "fileType":"Json",
+        "packageType":"Unsupported",
+        "sourcePackageId":"N:package:98757b39-b18e-4940-82f3-f84b4856fce3",
+        "createdAt":null,
+        "type":"File",
+        "downloadUrl": "https://blackfynn-discover-use1.s3.amazonaws.com/84/1/files/template.json?AWSAccessKeyId=AKIAQNJEWKCFAOLGQTY6&Signature=8XQoMGyMXZ66h1SgUu4ZdnM5JGc%3D&x-amz-request-payer=requester&Expires=1604931672"
+    }
 ];
 populateTable(filesTable, tableData);
 
