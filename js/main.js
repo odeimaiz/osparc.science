c2bdf5bb6e68ea44211daa03774325a41b20662e
odeimaiz <maiz@itis.swiss>
2020-11-09 11:08:40 +0100

    createTable and populateTable

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function create_image(src, width, height, alt) {
+function createImage(src, width, height, alt) {
     const img = document.createElement("img");
     img.src = src;
     // img.width = width;
@@ -7,8 +7,46 @@ function create_image(src, width, height, alt) {
     return img;
 }
 
-const header = create_image("resources/header.png");
+function createTable() {
+    const tbl = document.createElement("table");
+    tbl.style.width = "100%";
+
+    const theader = tbl.createTHead();
+    const header = theader.insertRow(0);
+    header.insertCell(0).innerHTML = "<b>Name</b>";
+    header.insertCell(1).innerHTML = "<b>File type</b>";
+    header.insertCell(2).innerHTML = "<b>Size</b>";
+    header.insertCell(3).innerHTML = "<b>Operation</b>";
+
+    return tbl;
+}
+
+function populateTable(tbl, data) {
+    const tbdy = document.createElement("tbody");
+    for (let i = 0; i < data.length; i++) {
+        const rowData = data[i];
+        const tr = tbl.insertRow();
+        for (let j = 0; j < 4; j++) {
+            const td = tr.insertCell();
+            td.appendChild(document.createTextNode(rowData[j]));
+        }
+        tbdy.appendChild(tr);
+    }
+    tbl.appendChild(tbdy);
+}
+
+const header = createImage("resources/header.png");
 document.body.appendChild(header);
 
-const footer = create_image("resources/footer.png");
+
+const filesTable = createTable();
+document.body.appendChild(filesTable);
+
+const tableData = [
+    ["template.json", "Json", "51B", "Open"]
+];
+populateTable(filesTable, tableData);
+
+
+const footer = createImage("resources/footer.png");
 document.body.appendChild(footer);
